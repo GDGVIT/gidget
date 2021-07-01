0d38319d9a93b95f5034ade89039ec553b4f3fa4
Rishav Naskar <rishavnaskar.r@gmail.com>
2021-07-01 14:49:53 +0530

    fix: linting errors

--- app/src/main/java/com/dscvit/gidget/common/Utils.kt
+++ app/src/main/java/com/dscvit/gidget/common/Utils.kt
@@ -12,7 +12,6 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.RectF
 import android.graphics.Shader
-import android.os.Build
 import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.preference.PreferenceManager
